3973af4ecb09e69b08d9c8c40fc208d4bdaee155
Florian <m.morel.florian@gmail.com>
2012-09-28 14:45:50 +0200

    - Check if it's been committed correctly bichc

---
+++ SandboxProject/SandboxProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SandboxProject
+//
+//  Created by Florian Morel on 9/28/12.
+//  Copyright (c) 2012 Florian Morel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Bastien PD!\n";
+    return 0;
+}
+
